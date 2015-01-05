1de44378f718558728e8cb1bedd333b6aaa04898
Douglas Bates <dmbates@gmail.com>
2015-01-05 12:34:53 -0600

    Remove the deps.jl file as it is generated on package build.

--- deps/deps.jl
+++
@@ -1,8 +0,0 @@
-# This is an auto-generated file; do not edit
-
-const libR="/usr/lib/R/lib/libR.so"
-ENV["R_HOME"]="/usr/lib/R"
-ENV["R_DOC_DIR"]="/usr/share/R/doc"
-ENV["R_INCLUDE_DIR"]="/usr/share/R/include"
-ENV["R_SHARE_DIR"]="/usr/share/R/share"
-ENV["LD_LIBRARY_PATH"]="/usr/lib/R/lib:/usr/lib/x86_64-linux-gnu:/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server"
