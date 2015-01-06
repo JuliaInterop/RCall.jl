8f1f2144b91f146c659009b7364182b40c304f7e
Douglas Bates <dmbates@gmail.com>
2015-01-06 11:27:07 -0600

    Add documentation on what build.jl is doing.

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,11 @@
+## Determine the value of R_HOME from the environment or through evaluating `R RHOME`
 const rh = haskey(ENV,"R_HOME") ? ENV["R_HOME"] : (ENV["R_HOME"] = chomp(readall(`R RHOME`)))
+
+## Create and test the libR path
 const libR = joinpath(rh,"lib",string("libR.",Base.Sys.shlib_ext))
 dlopen_e(libR) == C_NULL && error("Unable to load $libR\n\nPlease re-run Pkg.build(package), and restart Julia.")
+
+## Check for the values of other environment variables expected by R
 envset = true
 const envkeys = ["R_HOME","R_DOC_DIR","R_INCLUDE_DIR","R_SHARE_DIR","LD_LIBRARY_PATH"]
 for kk in envkeys
@@ -9,12 +14,15 @@ for kk in envkeys
         break
     end
 end
-if !envset
+
+if !envset                              # extract the values through Rscript
     vals = split(readall(`Rscript -e 'for (nm in c("R_HOME","R_DOC_DIR","R_INCLUDE_DIR","R_SHARE_DIR","LD_LIBRARY_PATH")) print(Sys.getenv(nm),quote=FALSE)'`),'\n')
     for i in 1:length(envkeys)
         ENV[envkeys[i]] = vals[i][5:end]  # first part of each value is "[1] "
     end
-end      
+end
+
+## Write the deps.jl file
 open("./deps.jl","w") do io
     println(io,"# This is an auto-generated file; do not edit\n")
     println(io,"const libR=\"",libR,"\"")
