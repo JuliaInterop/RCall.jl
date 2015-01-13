9a24de95a9e351fe4291ecff46e018b865083d0d
Douglas Bates <dmbates@gmail.com>
2015-01-13 14:46:31 -0600

    Use PreserveObject in asSEXP
    Also
     - use symbolic constants instead of explicit integer values for `SEXPREC` types
     - define `length` and `size` methods for SEXP{N} using metaprogramming
     - define `asSEXP` methods for various number types and for arrays of number types
        using metaprogramming.

--- src/sexp.jl
+++ src/sexp.jl
@@ -1,40 +1,22 @@
-# not sure these methods are needed, perhaps just a comment will do.
-SXPtype(s::SEXP{0}) = :NILSXP        # NULL in R
-SXPtype(s::SEXP{1}) = :SYMSXP        # R Symbol
-SXPtype(s::SEXP{2}) = :LISTSXP       # internal "pairs list", the R list type is 19
-SXPtype(s::SEXP{3}) = :CLOSXP        # closures
-SXPtype(s::SEXP{4}) = :ENVSXP        # environments
-SXPtype(s::SEXP{5}) = :PROMSX        # promises: [un]evaluated closure arguments
-SXPtype(s::SEXP{6}) = :LANGSXP       # language constructs (special lists)
-SXPtype(s::SEXP{7}) = :SPECIALSXP    # special forms
-SXPtype(s::SEXP{8}) = :BUILTINSXP    # builtin non-special forms
-SXPtype(s::SEXP{9}) = :CHARSXP       # "scalar" string type (internal only)
-SXPtype(s::SEXP{10}) = :LGLSXP        # logical vectors (but can contain NA's)
-## 11 and 12 were factors and ordered factors in the 1990's
-SXPtype(s::SEXP{13}) = :INTSXP        # Int32 vectors
-SXPtype(s::SEXP{14}) = :REALSXP       # Float64 vectors
-SXPtype(s::SEXP{15}) = :CPLXSXP       # Complex128 vectors
-SXPtype(s::SEXP{16}) = :STRSXP        # string vectors
-SXPtype(s::SEXP{17}) = :DOTSXP        # dot-dot-dot object
-SXPtype(s::SEXP{18}) = :ANYSXP        # make "any" args work
-SXPtype(s::SEXP{19}) = :VECSXP        # generic vectors (i.e. R's list type)
-SXPtype(s::SEXP{20}) = :EXPSXP        # expression vectors (as returned by parse)
-SXPtype(s::SEXP{21}) = :BCODESXP      # byte code
-SXPtype(s::SEXP{22}) = :EXTPTRSXP     # external pointer
-SXPtype(s::SEXP{23}) = :WEAKREFSXP    # weak references
-SXPtype(s::SEXP{24}) = :RAWSXP        # raw bytes
-SXPtype(s::SEXP{25}) = :S4SXP          # S4 non-vector
-
+## Methods related to the SEXP (pointer to SEXPREC type) in R
 
 ## length methods for the vector types of SEXP's.  To handle long vectors the length method
 ## should be extended to check for the sentinal value (typemin(Cint)) followed by extraction
-## of the Int64 length.
-for N in [10,13:16,19,20]
-    @eval Base.length(s::SEXP{$N}) = unsafe_load(convert(Ptr{Cint},s.p+loffset),1)
+## of the Int64 length.  The size methods check for a dim attribute and return a tuple.
+for N in [LGLSXP,INTSXP,REALSXP,CPLXSXP,STRSXP,VECSXP,EXPRSXP]
+    @eval begin
+        Base.length(s::SEXP{$N}) = unsafe_load(convert(Ptr{Cint},s.p+loffset),1)
+        function Base.size(s::SEXP{$N})
+            dd = Reval(lang2(dimSymbol,s))
+            isa(dd,SEXP{INTSXP}) || return (int64(length(s)),)
+            vv = vec(dd)
+            ntuple(length(vv),i->int64(vv[i]))
+        end
+    end
 end
 
 ## Element extraction and iterators
-for (N,elt) in ((16,:STRING_ELT),(19,:VECTOR_ELT),(20,:VECTOR_ELT))
+for (N,elt) in ((STRSXP,:STRING_ELT),(VECSXP,:VECTOR_ELT),(EXPRSXP,:VECTOR_ELT))
     @eval begin
         function Base.getindex(s::SEXP{$N},I::Number)  # extract a single element
             0 < I ≤ length(s) || throw(BoundsError())
@@ -46,7 +28,7 @@ for (N,elt) in ((16,:STRING_ELT),(19,:VECTOR_ELT),(20,:VECTOR_ELT))
         Base.eltype(s::SEXP{$N}) = SEXP
     end
 end
-Base.eltype(s::SEXP{16}) = SEXP{9}      # be more specific for STRSXP
+Base.eltype(s::SEXP{STRSXP}) = SEXP{CHARSXP} # be more specific for STRSXP
 
 ## ToDo: write an isNA method for Complex128 - not sure how it is defined though.
 isNA(x::Cdouble) = x == R_NaReal
@@ -55,27 +37,16 @@ isNA(x::Union(ASCIIString,UTF8String)) = x == bytestring(R_NaString)
 isNA(a::Array) = reshape(bitpack([isNA(aa) for aa in a]),size(a))
 
 ## bytestring copies the contents of the 0-terminated string at the Ptr{Uint8} address
-Base.bytestring(s::SEXP{9}) = bytestring(ccall((:R_CHAR,libR),Ptr{Uint8},(Ptr{Void},),s.p))
+Base.bytestring(s::SEXP{CHARSXP}) = bytestring(ccall((:R_CHAR,libR),Ptr{Uint8},(Ptr{Void},),s))
 
-for N in [10,13:16,19,20]
-    @eval begin
-        function Base.size(s::SEXP{$N})
-            dd = Reval(lang2(dimSymbol,s))
-            isa(dd,SEXP{13}) || return (int64(length(s)),)
-            vv = vec(dd)
-            ntuple(length(vv),i->int64(vv[i]))
-        end
-    end
-end
+Base.vec(s::SEXP{STRSXP}) = [bytestring(ss) for ss in s]
 
-Base.vec(s::SEXP{16}) = [bytestring(ss) for ss in s]
-
-for (N,typ) in ((10,:Int32),(13,:Int32),(14,:Float64),(15,:Complex128))
+for (N,typ) in ((LGLSXP,:Int32),(INTSXP,:Int32),(REALSXP,:Float64),(CPLXSXP,:Complex128))
     @eval begin
         function Base.vec(s::SEXP{$N})
             ccall((:R_PreserveObject,libR),Void,(Ptr{Void},),s)
             rv = pointer_to_array(convert(Ptr{$typ},s.p+voffset),length(s))
-            finalizer(rv,x->ccall((:R_ReleaseObject,libR),Void,(Ptr{Void},),s.p))
+            finalizer(rv,x->ccall((:R_ReleaseObject,libR),Void,(Ptr{Void},),s))
             rv
         end
     end
@@ -84,7 +55,7 @@ end
 ## Not sure what to do about R's Logical vectors (SEXP{10}) They are stored as Int32 and can
 ## have missing values.  The DataArray method must copy the values if it is to produce Bool's.
 ## For the time being, I will leave them as Int32's.
-for N in [10,13:16]
+for N in [LGLSXP,INTSXP,REALSXP,CPLXSXP,STRSXP]
     @eval begin
         DataArrays.DataArray(s::SEXP{$N}) = (rv = reshape(vec(s),size(s));DataArray(rv,isNA(rv)))
         dataset(s::SEXP{$N}) = DataArray(s)
@@ -93,59 +64,71 @@ end
 
 ## ToDo: expand these definitions to check for names and produce NamedArrays?
 
-function Base.replace{T<:Number}(v::AbstractArray{T},pat::T,r::T)
-    for i in 1:length(v)
-        v[i] == pat && (v[i] = r)
-    end
-    v
-end
-
-# make dataset more general for integer vectors, which can be factors
-@doc "dataset method for SEXP{13} must check if the argument is a factor"->
-function dataset(s::SEXP{13})
+@doc "dataset method for SEXP{INTSXP} must check if the argument is a factor"->
+function dataset(s::SEXP{INTSXP})
     isFactor(s) || return DataArray(s)
-    refs = DataArrays.RefArray(replace(vec(s),R_NaInt,zero(Int32)))
+    ## refs array uses a zero index where R has a missing value, R_NaInt
+    refs = DataArrays.RefArray(map!(x -> x == R_NaInt ? zero(Int32) : x,vec(s)))
     compact(PooledDataArray(refs,R.levels(s)))
 end
 
 Base.names(s::SEXP) = vec(asSEXP(ccall((:Rf_getAttrib,libR),Ptr{Void},
                                        (Ptr{Void},Ptr{Void}),s,namesSymbol)))
 
-function dataset(s::SEXP{19})
+function dataset(s::SEXP{VECSXP})
     val = [dataset(v) for v in s]
     R.inherits(s,"data.frame") ? DataFrame(val,Symbol[symbol(nm) for nm in names(s)]) : val
 end
 
-@doc "convert a symbol or ASCIIString to a dataset"->
+@doc "Evaluate Symbol s as an R dataset"->
 dataset(s::Symbol) = dataset(Reval(s))
 
-@doc "assign value v to symbol s in the environment e"->
-Base.setindex!(e::SEXP{4},v::SEXP,s::Symbol) =
-    ccall((:Rf_setVar,libR),Void,(Ptr{Void},Ptr{Void},Ptr{Void}),install(s),v,e)
-Base.setindex!{T<:Number}(e::SEXP{4},v::Array{T},s::Symbol) = setindex!(e,asSEXP(v),s)
-Base.setindex!(e::SEXP{4},v::Real,s::Symbol) = setindex!(e,asSEXP(v),s)
-
-
-Base.getindex(e::SEXP{4},s::Symbol) =
+@doc "extract the value of symbol s in the environment e"->
+Base.getindex(e::SEXP{ENVSXP},s::Symbol) =
     asSEXP(ccall((:Rf_findVarInFrame,libR),Ptr{Void},(Ptr{Void},Ptr{Void}),e,install(s)))
 
-asSEXP(v::Integer) = scalarInteger(v)
+@doc "assign value v to symbol s in the environment e"->
+Base.setindex!(e::SEXP{ENVSXP},v::SEXP,s::Symbol) =
+    ccall((:Rf_setVar,libR),Void,(Ptr{Void},Ptr{Void},Ptr{Void}),install(s),v,e)
+Base.setindex!{T<:Number}(e::SEXP{ENVSXP},v::Array{T},s::Symbol) = setindex!(e,asSEXP(v),s)
+Base.setindex!(e::SEXP{ENVSXP},v::Number,s::Symbol) = setindex!(e,asSEXP(v),s)
+Base.setindex!(e::SEXP{ENVSXP},v::Real,s::Symbol) = setindex!(e,asSEXP(v),s)
 
-asSEXP(v::Real) = scalarReal(v)
-
-asSEXP(v::Bool) = scalarLogical(v)
-
-function asSEXP{T<:Integer}(v::Vector{T})
-    l = length(v)
-    vv = asSEXP(ccall((:Rf_allocVector,libR),Ptr{Void},(Cint,Cptrdiff_t),13,l))
-    copy!(pointer_to_array(convert(Ptr{Cint},vv.p+voffset),l),v)
-    vv
+function preserve(s::SEXP)
+    ccall((:R_PreserveObject,libR),Void,(Ptr{Void},),s)
+    finalizer(s,x -> ccall((:R_ReleaseObject,libR),Void,(Ptr{Void},),x))
+    s
 end
 
-function asSEXP{T<:Real}(v::Vector{T})
-    l = length(v)
-    vv = asSEXP(ccall((:Rf_allocVector,libR),Ptr{Void},(Cint,Cptrdiff_t),14,l))
-    copy!(pointer_to_array(convert(Ptr{Cdouble},vv.p+voffset),l),v)
-    vv
+for (typ,rnm,tag,rtyp) in ((:Bool,:Logical,LGLSXP,:Int32),
+                           (:Complex128,:Complex,CPLXSXP,:Complex128),
+                           (:Integer,:Integer,INTSXP,:Int32),
+                           (:Real,:Real,REALSXP,:Float64))
+    @eval begin
+        asSEXP(v::$typ) = preserve($(symbol(string("scalar",rnm)))(v)) # scalarInteger, etc.
+        function asSEXP{T<:$typ}(v::Vector{T})
+            l = length(v)
+            vv = asSEXP(ccall((:Rf_allocVector,libR),Ptr{Void},(Cint,Cptrdiff_t),$tag,l))
+            copy!(pointer_to_array(convert(Ptr{$rtyp},vv.p+voffset),l),v)
+            preserve(vv)
+        end
+        function asSEXP{T<:$typ}(m::Matrix{T})
+            p,q = size(m)
+            vv = asSEXP(ccall((:Rf_allocMatrix,libR),Ptr{Void},(Cint,Cint,Cint),$tag,p,q))
+            copy!(pointer_to_array(convert(Ptr{$rtyp},vv.p+voffset),l),m)
+            preserve(vv)
+        end
+        function asSEXP{T<:$typ}(a::Array{T,3})
+            p,q,r = size(a)
+            vv = asSEXP(ccall((:Rf_alloc3DArray,libR),Ptr{Void},(Cint,Cint,Cint,Cint),$tag,p,q,r))
+            copy!(pointer_to_array(convert(Ptr{$rtyp},vv.p+voffset),l),a)
+            preserve(vv)
+        end
+        function asSEXP{T<:$typ}(a::Array{T})
+            rdims = asSEXP([size(a)...])
+            vv = asSEXP(ccall((:Rf_allocArray,libR),Ptr{Void},(Cint,Ptr{Void}),$tag,rdims))
+            copy!(pointer_to_array(convert(Ptr{$rtyp},vv.p+voffset),l),a)
+            preserve(vv)
+        end
+    end
 end
-
