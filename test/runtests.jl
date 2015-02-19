45f206c17827b121b03b86e6f64cbc59ee45a3b6
Douglas Bates <dmbates@gmail.com>
2015-02-19 14:56:19 -0600

    Update tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,16 @@
 using RCall
 using Base.Test,DataArrays,DataFrames
 
-lsv = R.ls(;printR=false)
+lsv = reval("ls()")
 @test length(lsv) == 0
 @test isa(lsv, SEXP{16})
 
-lsd = R.ls("datasets";printR= false)
+lsd = reval("ls(\"package:datasets\")")
+@test isa(lsv, SEXP{16})
 @test length(lsd) == 103
-@test RCall.copyvec(lsd)[2] == "airmiles"
+@test bytestring(lsd[2]) == "airmiles"
 
-v110 = dataset(reval(rparse("x <- 1:10")))
+v110 = dataset(reval("x <- 1:10"))
 @test isa(v110,DataVector)
 @test eltype(v110) == Cint
 
