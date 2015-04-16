300d554ef4add3be8276c5dac73b00e941b22360
Simon Byrne <simonbyrne@gmail.com>
2015-04-16 10:25:46 +0100

    Improve IJulia functionality, update graphics docs

--- src/IJulia.jl
+++ src/IJulia.jl
@@ -3,7 +3,7 @@ import IPythonDisplay: InlineDisplay
 
 export rplot_set
 
-const rplot_active = Bool[false]
+const rplot_devno = Int32[1]
 const rplot_file = tempname()
 
 const rplot_opts = Any[MIME"image/png"(),(480,400),()]
@@ -30,16 +30,18 @@ rplot_set(m::MIME"image/svg+xml") = rplot_set(m,6,5)
 rplot_device(m::MIME"image/png") = :png
 rplot_device(m::MIME"image/svg+xml") = :svg
 
-
-# open new png device
+@doc """
+Called before cell evaluation. 
+Opens new graphics device.
+"""->
 function new_rplot()
     rcall(rplot_device(rplot_opts[1]),rplot_file,rplot_opts[2]...;rplot_opts[3]...)
-    rplot_active[1] = true
+    rplot_devno[1] = rcopy(rcall(symbol("dev.cur")))[1]
 end
 
 function displayfile(m::MIME"image/png", f)
     open(f) do f
-        d = read(f,UInt8,filesize(rplot_file))
+        d = readbytes(f)
         display(InlineDisplay(),m,d)
     end
 end
@@ -50,11 +52,15 @@ function displayfile(m::MIME"image/svg+xml", f)
     end
 end
 
-# close and display png device
+@doc """
+Called after cell evaluation.
+Closes graphics device and displays file in notebook.
+"""->
 function disp_rplot()
-    if rplot_active[1]
-        rcall(symbol("dev.off"))
-        rplot_active[1] = false
+    d = rplot_devno[1] 
+    if d != 1
+        rcall(symbol("dev.off"),d)
+        rplot_devno[1] = rcopy(rcall(symbol("dev.cur")))[1]
         if isfile(rplot_file)
             displayfile(rplot_opts[1],rplot_file)
             rm(rplot_file)
@@ -64,9 +70,11 @@ end
 
 # cleanup png device on error
 function clean_rplot()
-    if rplot_active[1]
-        rcall(symbol("dev.off"))
-        rplot_active[1] = false
+    d = rplot_devno[1] 
+    if d != 1
+        rprint("dev.list()")
+        rcall(symbol("dev.off"),d)
+        rplot_devno[1] = rcopy(rcall(symbol("dev.cur")))[1]
     end
     isfile(rplot_file) && rm(rplot_file)
 end
