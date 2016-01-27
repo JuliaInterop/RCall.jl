cce789a723df5b9815ab9308731046bbde358f0c
ranjanan <benditlikeranjan@gmail.com>
2016-01-27 16:14:14 +0530

    Add more tests to improve code coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,3 +20,32 @@ for t in tests
     println(" * $(tfile) ...")
     include(tfile)
 end
+
+#More tests
+a = RObject(rand(10))
+@test length(rcopy(Any, a)) == 10
+@test typeof(RCall.sexp(Cint, 1)) == Cint
+@test typeof(RCall.sexp(Float64, 1)) == Float64
+@test typeof(RCall.sexp(Complex128, 1)) == Complex128
+@test typeof(rcopy(Vector{Float64}, a.p)) == Vector{Float64}
+b = RObject(true)
+@test rcopy(Cint(1)) == 1
+@test rcopy(Cint, Cint(1)) == 1
+@test rcopy(Cint, b.p) == 1
+@test rcopy(Vector{Cint}, b.p) == [1]
+@test rcopy(Array{Cint}, b.p) == [1]
+@test rcopy(Vector{Bool}, b.p) == [true]
+@test rcopy(BitVector, b.p) == [true]
+function funk(x,y)
+    x+y
+end
+f1 = RObject(funk)
+@test rcopy(Function, f1)(1,2) == 3
+@test rcopy(Function, f1.p)(1,2) == 3 
+#RCall.rlang_formula(parse("a+b"))
+@test RCall.rlang_formula(:a) == :a
+
+#Dictionaries
+d = RObject(Dict(1=>2)) 
+@test Dict{Any,Any}("1" => 2) == rcopy(Dict, d)
+@test Dict{Int,Int}(1=>2) == rcopy(Dict{Int,Int}, d)
