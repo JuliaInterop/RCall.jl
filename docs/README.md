b149dd49481aed91b667d818cd64f4b623ed5155
Randy Lai <randy.cs.lai@gmail.com>
2016-03-14 16:56:40 -0400

    automatic deploy latest documentation

--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,6 @@
 ### Building Documentations
 
 [Lapidary](https://github.com/MichaelHatherly/Lapidary.jl) is used to generate the documentations automatically.
-Due to a bug of Lapidary, julia 0.5 is required.
 
 ```bash
 # install mkdocs if necessary
--- docs/make.jl
+++ docs/make.jl
@@ -8,3 +8,12 @@ makedocs(
     modules = [RCall],
     clean   = true
 )
+
+# Needs to install an additional dep, mkdocs-material, so provide a custom `deps`.
+custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material`)
+
+deploydocs(
+    # options
+    deps = custom_deps,
+    repo = "github.com/JuliaStats/RCall.jl.git"
+)
