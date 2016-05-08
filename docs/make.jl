c4a542a14a6d318c7aba1442d0ba04b4e2fe01cb
Simon Byrne <simonbyrne@gmail.com>
2016-05-08 16:43:56 +0100

    use release in docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,4 @@ using Documenter, RCall
 
 makedocs(modules = [RCall], clean = true)
 
-deploydocs(repo = "github.com/JuliaStats/RCall.jl.git")
+deploydocs(repo = "github.com/JuliaStats/RCall.jl.git", julia  = "release")
