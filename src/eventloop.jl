f46481e462d17ac0bfc5a02fcd80f00637bccd88
Douglas Bates <dmbates@gmail.com>
2016-06-10 14:09:52 -0500

    Fix logic re platforms (caught by randy3k)

--- src/eventloop.jl
+++ src/eventloop.jl
@@ -1,10 +1,10 @@
 function process_events()
     ##FIXME: a dirty fix to prevent segfault right after a sigint
     if unsafe_load(cglobal((:R_interrupts_pending,libR),Cint)) == 0
-        if Compat.is_unix()
+        if Compat.is_windows() || Compat.is_apple()
             ccall((:R_ProcessEvents, libR), Void, ())
         end
-        if Compat.is_windows()
+        if Compat.is_unix()
             what = ccall((:R_checkActivity,libR),Ptr{Void},(Cint,Cint),0,1)
             if what != C_NULL
                 R_InputHandlers = unsafe_load(cglobal((:R_InputHandlers,libR),Ptr{Void}))
