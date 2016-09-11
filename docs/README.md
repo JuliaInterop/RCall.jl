e2d169e327d24777f215741ed2b4942beced1f33
Randy Lai <randy.cs.lai@gmail.com>
2016-09-11 16:40:53 -0400

    Update Documenter settings

--- docs/README.md
+++ docs/README.md
@@ -5,11 +5,13 @@ the documentations automatically. Lastest documentations of the master branch
 and documentataions of tags are generated and deployed automatically to gh-
 pages.
 
+
 To view the docs locally, you first need to install Documenter.jl:
 
-    julia -e 'Pkg.clone("https://github.com/MichaelHatherly/Documenter.jl")'
+    julia -e 'Pkg.add("Documenter")'
 
 then run the following from within this directory:
 
     julia make.jl
-    mkdocs serve
+
+HTML files are generated in the `build` directory.
--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,19 @@
 using Documenter, RCall
 
-makedocs(modules = [RCall], clean = true)
+makedocs(
+    modules = [RCall],
+    clean   = false,
+    format   = Documenter.Formats.HTML,
+    sitename = "RCall.jl",
+    pages    = Any[
+        "Introduction" => "index.md",
+        "Installation" => "installation.md",
+        "Getting Started" => "gettingstarted.md",
+        "Internal" => "internal.md"
+    ]
+)
 
-deploydocs(repo = "github.com/JuliaStats/RCall.jl.git", julia  = "release")
+deploydocs(
+    repo = "github.com/JuliaStats/RCall.jl.git",
+    julia  = "0.4"
+)
