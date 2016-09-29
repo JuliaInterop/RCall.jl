645ba896ba2259d37e9d115ea56483e7c41e9b29
Randy Lai <randy.cs.lai@gmail.com>
2016-09-29 13:15:39 -0400

    Fix deploydocs repo

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,7 @@ makedocs(
 )
 
 deploydocs(
-    repo = "github.com/JuliaStats/RCall.jl.git",
+    repo = "github.com/JuliaInterop/RCall.jl.git",
     julia  = "0.5",
     target = "build",
     deps = nothing,
