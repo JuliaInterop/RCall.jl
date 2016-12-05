69019bcd7669caa01d3885f54bd9d666036a78f2
Simon Byrne <simonbyrne@gmail.com>
2016-12-05 17:00:24 +0000

    change method used by getclass

--- test/basic.jl
+++ test/basic.jl
@@ -156,3 +156,12 @@ model <- lm(Sepal.Width ~ Petal.Length, iris)
 """
 model = rcopy(R"model")
 @test typeof(model[:call]) <: RObject
+
+# getclass
+@test rcopy(getclass(reval("1"))) == "numeric"
+@test rcopy(getclass(reval("1L"))) == "integer"
+@test rcopy(getclass(reval("complex(1,2)"))) == "complex"
+@test rcopy(getclass(reval("matrix(1)"))) == "matrix"
+@test rcopy(getclass(reval("function(x) x"))) == "function"
+@test rcopy(getclass(reval("data.frame(x=1)"))) == "data.frame"
+@test rcopy(getclass(reval("quote(zzz)"))) == "name"
