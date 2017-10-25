20e5a43219ebb8d4db41f5e61f31b54e051ab39f
Simon Byrne <simonbyrne@gmail.com>
2017-10-24 22:17:16 -0700

    Correctly set HOME/R_USER env vars in Windows (#207)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,33 @@ using Base.Test
 hd = homedir()
 pd = Pkg.dir()
 
+if is_windows()
+    Rhome = if haskey(ENV,"R_HOME")
+        ENV["R_HOME"]
+    else
+        using WinReg
+        WinReg.querykey(WinReg.HKEY_LOCAL_MACHINE, "Software\\R-Core\\R","InstallPath")
+    end
+    Rscript = joinpath(Rhome,"bin",Sys.WORD_SIZE==64?"x64":"i386","Rscript")
+else
+    Rscript = "Rscript"
+end
+
+libpaths = readlines(`$Rscript -e "writeLines(.libPaths())"`)
+if VERSION < v"0.6.0"
+    libpaths = map(chomp, libpaths)
+end
+
+
 using RCall
 using Compat
 
 # https://github.com/JuliaStats/RCall.jl/issues/68
 @test hd == homedir()
 @test pd == Pkg.dir()
+
+# https://github.com/JuliaInterop/RCall.jl/issues/206
+@test rcopy(Vector{String}, reval(".libPaths()")) == libpaths
 
 tests = ["basic",
          "convert/base",
