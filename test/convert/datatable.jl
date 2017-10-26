9c795a58219aa653709643f46e5cfe311fd6419f
Randy Lai <randy.cs.lai@gmail.com>
2017-10-26 16:02:47 -0400

    add tests for CategoricalArrays

--- test/convert/datatable.jl
+++ test/convert/datatable.jl
@@ -89,5 +89,12 @@ if Pkg.installed("CategoricalArrays") < v"0.2.0"
     @test CategoricalArrays.levels(rcopy(NullableCategoricalArray,R"factor(c('a',NA,'c'))")) == ["a","c"]
     @test CategoricalArrays.isordered(rcopy(NullableCategoricalArray,R"ordered(c('a',NA,'c'))"))
 else
-
+    a = Array{Union{Null, String}}(repeat(["a", "b"], inner = 5))
+    a[repeat([true, false], outer = 5)] = null
+    v = CategoricalArray(a)
+    @test isequal(rcopy(CategoricalArray,RObject(v)), v)
+    v = CategoricalArray(a, ordered=true)
+    @test isequal(rcopy(CategoricalArray,RObject(v)), v)
+    @test CategoricalArrays.levels(rcopy(CategoricalArray,R"factor(c('a',NA,'c'))")) == ["a","c"]
+    @test CategoricalArrays.isordered(rcopy(CategoricalArray,R"ordered(c('a',NA,'c'))"))
 end
