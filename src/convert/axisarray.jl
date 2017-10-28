6393d1a46f5638073ec444a0bed07dd3dbeb0eb9
Randy Lai <randy.cs.lai@gmail.com>
2017-10-28 15:40:54 -0400

    improve axisarrays

--- src/convert/axisarray.jl
+++ src/convert/axisarray.jl
@@ -1,9 +1,34 @@
-function rcopy(::Type{AxisArray}, r::Ptr{S}) where S<:VectorSxp
+for (J,S) in ((:Int,:IntSxp),
+                 (:Float64, :RealSxp),
+                 (:Complex128, :CplxSxp),
+                 (:Bool, :LglSxp),
+                 (:String, :StrSxp),
+                 (:UInt8, :RawSxp))
+    @eval begin
+        function rcopy(::Type{AxisArray},s::Ptr{$S})
+            protect(s)
+            try
+                class = rcopy(Symbol, getclass(s, true))
+                if method_exists(eltype, Tuple{Type{RClass{class}}, Ptr{$S}})
+                    return rcopy(AxisArray{eltype(RClass{class}, s)}, s)
+                else
+                    return rcopy(AxisArray{$J},s)
+                end
+            finally
+                unprotect(1)
+            end
+        end
+    end
+end
+
+function rcopy(::Type{AxisArray{T}}, r::Ptr{S}) where {S<:VectorSxp, T}
     dnames = getattrib(r, Const.DimNamesSymbol)
     isnull(dnames) && error("r has no dimnames")
     dsym = rcopy(Array{Symbol}, getnames(dnames))
-    AxisArray(rcopy(DataArray, r), [Axis{dsym[i]}(rcopy(n)) for (i,n) in enumerate(dnames)]...)
+    AxisArray(anyna(r) ? rcopy(DataArray{T}, r) : rcopy(Array{T}, r),
+             [Axis{dsym[i]}(rcopy(n)) for (i,n) in enumerate(dnames)]...)
 end
+
 
 for S in (:IntSxp, :RealSxp, :CplxSxp, :LglSxp, :StrSxp)
     @eval begin
