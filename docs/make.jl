04972d77722dcac2e645783c7b0bd848c64132b4
Randy Lai <randy.cs.lai@gmail.com>
2017-11-05 22:42:37 -0500

    make docs for julia 0.6

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,7 @@ makedocs(
 deploydocs(
     repo = "github.com/JuliaInterop/RCall.jl.git",
     target = "build",
-    julia  = "0.5",
+    julia  = "0.6",
     deps = nothing,
     make = nothing
 )
