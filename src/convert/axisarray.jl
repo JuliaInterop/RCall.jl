9706075042cd65366b4e0438e50c8725b88975c7
Randy Lai <randy.cs.lai@gmail.com>
2018-01-18 10:43:15 -0500

    support empty axis
    close #222

--- src/convert/axisarray.jl
+++ src/convert/axisarray.jl
@@ -2,8 +2,13 @@ function rcopy(::Type{AxisArray}, r::Ptr{S}) where {S<:VectorSxp}
     dnames = getattrib(r, Const.DimNamesSymbol)
     isnull(dnames) && error("r has no dimnames")
     dsym = rcopy(Array{Symbol}, getnames(dnames))
+    for i in 1:length(dsym)
+        if dsym[i] == Symbol("")
+            dsym[i] = i == 1 ? (:row) : i == 2 ? (:col) : i == 3 ? (:page) : Symbol(:dim_, i)
+        end
+    end
     AxisArray(rcopy(AbstractArray, r),
-             [Axis{dsym[i]}(rcopy(n)) for (i,n) in enumerate(dnames)]...)
+             [Axis{dsym[i]}(isnull(n) ? (1:(size(r)[i])) : rcopy(n)) for (i,n) in enumerate(dnames)]...)
 end
 
 function sexp(aa::AxisArray)
