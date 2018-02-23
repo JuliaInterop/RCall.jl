51d21cde377710a4fa35bff0c6d6280e59a0317e
Randy Lai <randy.cs.lai@gmail.com>
2018-02-23 13:23:58 -0500

    use Pkg.build to discover libR

--- src/setup.jl
+++ src/setup.jl
@@ -51,6 +51,9 @@
 
 end
 
+const Rembedded = Ref{Bool}(false)
+const voffset = Ref{UInt}()
+
 """
     validate_libR(libR)
 
@@ -82,48 +85,6 @@ function validate_libR(libR)
     end
     Libdl.dlclose(libptr)
 end
-
-function locate_Rhome()
-    Rhome = if haskey(ENV,"R_HOME")
-        ENV["R_HOME"]
-    else
-        try
-            readchomp(`R RHOME`)
-        catch er
-            ""
-        end
-    end
-    @static if Compat.Sys.iswindows()
-        if Rhome == ""
-            Rhome = try
-                    WinReg.querykey(WinReg.HKEY_LOCAL_MACHINE, "Software\\R-Core\\R", "InstallPath")
-                catch er
-                    ""
-                end
-        end
-    end
-    if Rhome == "" || !isdir(Rhome)
-        error("Could not find R installation. Either set the \"R_HOME\" environmental variable, or ensure the R executable is available in \"PATH\".")
-    end
-    info("Using R installation at $Rhome")
-    Rhome
-end
-
-function locate_libR(Rhome)
-    @static if Compat.Sys.iswindows()
-        libR = joinpath(Rhome, "bin", Sys.WORD_SIZE==64 ? "x64" : "i386", "R.dll")
-    else
-        libR = joinpath(Rhome, "lib", "libR.$(Libdl.dlext)")
-    end
-    validate_libR(libR)
-    libR
-end
-
-const Rhome = locate_Rhome()
-const libR = locate_libR(Rhome)
-const Rembedded = Ref{Bool}(false)
-const voffset = Ref{UInt}()
-
 
 """
     initEmbeddedR()
@@ -211,6 +172,9 @@ function endEmbeddedR()
         Rembedded[] = false
     end
 end
+
+const depfile = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
+include(depfile)
 
 function __init__()
     validate_libR(libR)
