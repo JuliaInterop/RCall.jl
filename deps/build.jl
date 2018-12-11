bf412905d494e69d808a4f1df04914bc6cb6bcfc
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2018-12-11 14:55:49 -0500

    better R_HOME handling

--- deps/build.jl
+++ deps/build.jl
@@ -19,34 +19,44 @@ try
         @info "Using previously configured R at $Rhome with libR in $libR."
     else
         Rhome = get(ENV, "R_HOME", "")
-        if isempty(Rhome)
-            try Rhome = readchomp(`R RHOME`); catch; end
-        end
-        @static if Sys.iswindows()
-            if isempty(Rhome)
-                try Rhome = WinReg.querykey(WinReg.HKEY_LOCAL_MACHINE,
-                                            "Software\\R-Core\\R", "InstallPath"); catch; end
-            end
+        if Rhome == "*"
+            # install with Conda
+            different = "  To use a different R installation, set the \"R_HOME\" environment variable and re-run Pkg.build(\"RCall\")."
+            @info "Installing R via Conda.$different"
+            Conda.add_channel("r")
+            Conda.add("r-base")
+            Rhome = joinpath(Conda.LIBDIR, "R")
+            libR = locate_libR(Rhome, false)
+            isempty(libR) && error("Conda R installation failed.$different")
         else
-            if Rhome != "*" && !isdir(Rhome)
-                error("R_HOME is not a directory.")
+            if isempty(Rhome)
+                try Rhome = readchomp(`R RHOME`); catch; end
             end
-        end
-        Rhome = Rhome == "*" ? "" : Rhome
-        libR = isempty(Rhome) ? "" : locate_libR(Rhome, false)
-        if isempty(libR)
-            if get(ENV, "R_HOME", "") == "*"
-                different = "  To use a different R installation, set the \"R_HOME\" environment variable and re-run Pkg.build(\"RCall\")."
-                @info "Installing R via Conda.$different"
-                Conda.add_channel("r")
-                Conda.add("r-base")
-                Rhome = joinpath(Conda.LIBDIR, "R")
-                libR = locate_libR(Rhome, false)
-                isempty(libR) && error("Conda R installation failed.$different")
+            @static if Sys.iswindows()
+                if isempty(Rhome)
+                    try Rhome = WinReg.querykey(WinReg.HKEY_LOCAL_MACHINE,
+                                                "Software\\R-Core\\R", "InstallPath"); catch; end
+                end
             else
-                error("Fail to use R at $Rhome.")
+                if !isdir(Rhome)
+                    error("R_HOME is not a directory.")
+                end
             end
         end
+        # libR = isempty(Rhome) ? "" : locate_libR(Rhome, false)
+        # if isempty(libR)
+        #     if get(ENV, "R_HOME", "") == "*"
+        #         different = "  To use a different R installation, set the \"R_HOME\" environment variable and re-run Pkg.build(\"RCall\")."
+        #         @info "Installing R via Conda.$different"
+        #         Conda.add_channel("r")
+        #         Conda.add("r-base")
+        #         Rhome = joinpath(Conda.LIBDIR, "R")
+        #         libR = locate_libR(Rhome, false)
+        #         isempty(libR) && error("Conda R installation failed.$different")
+        #     else
+        #         error("Fail to use R at $Rhome.")
+        #     end
+        # end
 
         @info "Using R at $Rhome and libR at $libR."
         if DepFile.Rhome != Rhome || DepFile.libR != libR
