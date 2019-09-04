adc63dddccdd16d34c0e39d8feeb27cae630cba0
lbilli <lbilli@users.noreply.github.com>
2019-09-04 12:46:21 -0400

    Allow conversions of Date and DateTime SubArrays

--- test/convert/datetime.jl
+++ test/convert/datetime.jl
@@ -23,6 +23,20 @@ r = RObject(d)
 @test rcopy(R"as.Date($s)") == d
 @test rcopy(R"identical(as.Date($s), $d)")
 
+
+# SubArray
+d = view(d, 1:2)
+r = RObject(d)
+@test isa(r,RObject{RealSxp})
+@test rcopy(getclass(r)) == "Date"
+@test length(r) == length(d)
+@test size(r) == size(d)
+@test rcopy(r) == d
+@test rcopy(Array, r) == d
+@test rcopy(R"as.Date($s[1:2])") == d
+@test rcopy(R"identical(as.Date($s[1:2]), $d)")
+
+
 d = Date[]
 r = RObject(d)
 @test isa(r,RObject{RealSxp})
@@ -61,6 +75,7 @@ r = RObject(d)
 @test rcopy(R"as.POSIXct($s, 'UTC', '%Y-%m-%dT%H:%M:%S')") == d
 @test rcopy(R"identical(as.character($d, '%Y-%m-%dT%H:%M:%S'), $s)")
 
+
 s = ["2001-01-01T01:01:01", "1111-11-11T11:11:00", "2012-12-12T12:12:12"]
 d = DateTime.(s)
 r = RObject(d)
@@ -75,6 +90,22 @@ r = RObject(d)
 @test rcopy(R"as.POSIXct($s, 'UTC', '%Y-%m-%dT%H:%M:%S')") == d
 @test rcopy(R"identical(as.character($d, '%Y-%m-%dT%H:%M:%S'), $s)")
 
+
+# SubArray
+d = view(d, 1:2)
+r = RObject(d)
+@test isa(r,RObject{RealSxp})
+@test rcopy(getclass(r)) == ["POSIXct", "POSIXt"]
+@test rcopy(getattrib(r, "tzone")) == "UTC"
+@test isa(rcopy(Array, r), Array{DateTime})
+@test length(r) == length(d)
+@test size(r) == size(d)
+@test rcopy(r) == d
+@test rcopy(Array{DateTime}, r) == d
+@test rcopy(R"as.POSIXct($s[1:2], 'UTC', '%Y-%m-%dT%H:%M:%S')") == d
+@test rcopy(R"identical(as.character($d, '%Y-%m-%dT%H:%M:%S'), $s[1:2])")
+
+
 d = DateTime[]
 r = RObject(d)
 @test isa(r,RObject{RealSxp})
@@ -86,9 +117,8 @@ r = RObject(d)
 @test rcopy(r) == d
 @test rcopy(R"as.POSIXct(character(0))") == Date[]
 
-# Missing array dateTime
 
-# Missing array date
+# Missing array dateTime
 d = [DateTime("2001-01-01T01:01:01"), missing, DateTime("2012-12-12T12:12:12")]
 r = RObject(d)
 @test isa(r,RObject{RealSxp})
