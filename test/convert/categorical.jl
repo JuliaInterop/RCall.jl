c1e1acc32834fa902e2dc27a78f10d2756263033
Milan Bouchet-Valat <nalimilan@club.fr>
2019-12-29 22:26:06 +0100

    Fix conversion of CategoricalArray to factor vs. ordered
    Ordered and unordered factors were swapped. Add tests to catch this.

--- test/convert/categorical.jl
+++ test/convert/categorical.jl
@@ -1,18 +1,28 @@
 using CategoricalArrays
 
 # CategoricalArrays
-v = CategoricalArray(repeat(["b", "a"], inner = 5))
-@test isequal(rcopy(CategoricalArray,RObject(v)), v)
-v = CategoricalArray(repeat(["b", "a"], inner = 5), ordered=true)
-@test isequal(rcopy(CategoricalArray,RObject(v)), v)
-@test CategoricalArrays.levels(rcopy(CategoricalArray,R"factor(c('c','a','a'))")) == ["a","c"]
-@test CategoricalArrays.isordered(rcopy(CategoricalArray,R"ordered(c('c','a','a'))"))
+for ord in (true, false)
+    v = CategoricalArray(repeat(["b", "a"], inner = 5), ordered=ord)
+    v2 = rcopy(CategoricalArray, RObject(v))
+    @test isequal(v2, v)
+    @test v2 isa CategoricalVector{String}
+    @test levels(v2) == levels(v)
+    @test isordered(v2) === ord
+end
+@test levels(rcopy(CategoricalArray, R"factor(c('c','a','a'))")) == ["a","c"]
+@test !isordered(rcopy(CategoricalArray, R"factor(c('c','a','a'))"))
+@test isordered(rcopy(CategoricalArray, R"ordered(c('c','a','a'))"))
 
 a = Array{Union{String, Missing}}(repeat(["b", "a"], inner = 5))
 a[repeat([true, false], outer = 5)] .= missing
-v = CategoricalArray(a)
-@test isequal(rcopy(CategoricalArray,RObject(v)), v)
-v = CategoricalArray(a, ordered=true)
-@test isequal(rcopy(CategoricalArray,RObject(v)), v)
-@test CategoricalArrays.levels(rcopy(CategoricalArray,R"factor(c('c',NA,'a'))")) == ["a","c"]
-@test CategoricalArrays.isordered(rcopy(CategoricalArray,R"ordered(c('c',NA,'a'))"))
+for ord in (true, false)
+    v = CategoricalArray(a, ordered=ord)
+    v2 = rcopy(CategoricalArray, RObject(v))
+    @test isequal(v2, v)
+    @test v2 isa CategoricalVector{Union{String,Missing}}
+    @test levels(v2) == levels(v)
+    v = CategoricalArray(a, ordered=true)
+end
+@test levels(rcopy(CategoricalArray, R"factor(c('c',NA,'a'))")) == ["a","c"]
+@test !isordered(rcopy(CategoricalArray, R"factor(c('c',NA,'a'))"))
+@test isordered(rcopy(CategoricalArray, R"ordered(c('c',NA,'a'))"))
