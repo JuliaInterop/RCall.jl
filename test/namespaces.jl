94f9d3f314f047c2ed13837a92fd9713b9546e7f
Dilum Aluthge <dilum@aluthge.com>
2020-09-21 02:13:14 -0400

    Fix and test the Conda-provided R. Specifically: 1. Build step: When installing R from Conda, require that R is greater than or equal to 3.4.0 AND strictly less than 4.0.0. 2. Build step: Define `RCall.conda_provided_r`, which is `true` if RCall is using the Conda-provided R, and `false` otherwise. 3. Test suite: fix some hard-coded instances of `R` and `Rscript` 4. Test suite: skip  the "namespaces" tests if the R "MASS" package is not installed. 5. Test suite: skip the `R_PPStackTop` test if we are using the Conda-provided R. 6. Travis CI: test both the Conda and non-Conda ways of getting R. 7. Travis CI: make sure that we always test the latest stable 1.x release of Julia 8. Travis CI: don't test on so many versions of Julia. Specifically, test on exactly three versions of Julia: 1.0 (which is currently the LTS), the latest stable 1.x release of Julia, and Julia nightly.

--- test/namespaces.jl
+++ test/namespaces.jl
@@ -6,11 +6,21 @@ module NamespaceTests
     using RCall
     using Test
 
-    @rimport MASS
-    @test rcopy(rcall(MASS.ginv, RObject([1 2; 0 4]))) ≈ [1 -0.5; 0 0.25]
-    @rimport MASS as mass
-    @test rcopy(rcall(mass.ginv, RObject([1 2; 0 4]))) ≈ [1 -0.5; 0 0.25]
-    @rlibrary MASS
-    @test rcopy(rcall(ginv, RObject([1 2; 0 4]))) ≈ [1 -0.5; 0 0.25]
+    RCall.R"""
+        has_mass_package = require("MASS")
+    """
+
+    RCall.@rget has_mass_package
+
+    @info "" has_mass_package
+
+    if has_mass_package # these tests will fail if the R "MASS" package is not already installed
+        @rimport MASS
+        @test rcopy(rcall(MASS.ginv, RObject([1 2; 0 4]))) ≈ [1 -0.5; 0 0.25]
+        @rimport MASS as mass
+        @test rcopy(rcall(mass.ginv, RObject([1 2; 0 4]))) ≈ [1 -0.5; 0 0.25]
+        @rlibrary MASS
+        @test rcopy(rcall(ginv, RObject([1 2; 0 4]))) ≈ [1 -0.5; 0 0.25]
+    end
 
 end
