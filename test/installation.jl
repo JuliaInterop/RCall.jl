eb19da1ae78af2a260c133c8d7e5adcdee500945
Phillip Alday <me@phillipalday.com>
2024-01-06 00:51:57 -0600

    test debugging

--- test/installation.jl
+++ test/installation.jl
@@ -9,35 +9,44 @@ const RCALL_DIR = dirname(@__DIR__)
 function test_installation(file, project=mktempdir())
     path = joinpath(@__DIR__, "installation", file)
     cmd = `$(Base.julia_cmd()) --project=$(project) $(path)`
-    return @test success(Cmd(cmd; env=Dict("RCALL_DIR" => RCALL_DIR)))
-end
-
-mktempdir() do dir
-  @testset "No R" begin
-      test_installation("rcall_without_r.jl", dir)
-  end
-  # We want to guard this with a version check so we don't run into the following
-  # (non-widespread) issue on older versions of Julia:
-  # https://github.com/JuliaLang/julia/issues/34276
-  # (related to incompatible libstdc++ versions)
-  @static if VERSION ≥ v"1.9"
-    @testset "Preferences" begin
-        test_installation("swap_to_prefs_and_condapkg.jl", dir)
+    cmd = Cmd(cmd; env=Dict("RCALL_DIR" => RCALL_DIR))
+    @test mktemp() do file, io
+        try
+            result = run(pipeline(cmd; stdout=io, stderr=io))
+            return success(result)
+        catch
+            @error open(f -> read(f, String), file)
+            return false
+        end
     end
-  end
 end
 
-# Test whether we can install RCall with Conda, and then switch to using
-# Preferences + CondaPkg
 mktempdir() do dir
-    @testset "Conda" begin
-        test_installation("install_conda.jl", dir)
+    @testset "No R" begin
+        test_installation("rcall_without_r.jl", dir)
     end
     # We want to guard this with a version check so we don't run into the following
     # (non-widespread) issue on older versions of Julia:
     # https://github.com/JuliaLang/julia/issues/34276
     # (related to incompatible libstdc++ versions)
     @static if VERSION ≥ v"1.9"
+        @testset "Preferences" begin
+            test_installation("swap_to_prefs_and_condapkg.jl", dir)
+        end
+    end
+end
+
+# We want to guard this with a version check so we don't run into the following
+# issue on older versions of Julia:
+# https://github.com/JuliaLang/julia/issues/34276
+# (related to incompatible libstdc++ versions)
+@static if VERSION ≥ v"1.9"
+    # Test whether we can install RCall with Conda, and then switch to using
+    # Preferences + CondaPkg
+    mktempdir() do dir
+        @testset "Conda" begin
+            test_installation("install_conda.jl", dir)
+        end
         @testset "Conda 2 Preferences" begin
             test_installation("swap_to_prefs_and_condapkg.jl", dir)
         end
