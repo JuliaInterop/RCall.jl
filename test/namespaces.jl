4a3f12f69b87a60f9f0b0376c22b46bbb710e908
Phillip Alday <phillip.alday@beacon.bio>
2024-07-17 00:29:47 +0200

    oh unicode

--- test/namespaces.jl
+++ test/namespaces.jl
@@ -21,18 +21,22 @@ module NamespaceTests
         @test rcopy(rcall(ginv, RObject([1 2; 0 4]))) ≈ [1 -0.5; 0 0.25]
     end
 
-    if !rcopy(reval("""require("ape")""")) # 418
-        @info "installing ape to temporary lib"
-        tmp = mktempdir()
-        reval("""lib <- "$(tmp)"
-                 .libPaths(lib)
-                 install.packages("ape", repos="https://cloud.r-project.org", method="wget", lib=lib)
-                 library("ape")""")
+    # why exclude Windows? because the tempdir could potentially include
+    # unicode characters and R can't handle that
+    if !Sys.iswindows()
+        if !rcopy(reval("""require("ape")"""))  # 418
+            @info "installing ape to temporary lib"
+            tmp = mktempdir()
+            reval("""lib <- "$(tmp)"
+                     .libPaths(lib)
+                     install.packages("ape", repos="https://cloud.r-project.org", method="wget", lib=lib)
+                     library("ape")""")
+        end
+
+        @test_throws ErrorException rimport("ape")
+
+        rimport(:ape; normalization=["." => "__"])
     end
-
-    @test_throws ErrorException rimport("ape")
-
-    rimport(:ape; normalization=["." => "__"])
     # stats is always available
     rimport(:stats; normalizenames=false)
 end
