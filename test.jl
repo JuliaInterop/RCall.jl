f9baa587ca1be1eea6db2cfbc3b2affbaf1288b3
Phillip Alday <me@phillipalday.com>
2025-04-18 15:28:55 -0500

    more test tweaks

--- test.jl
+++ test.jl
@@ -1,6 +1,7 @@
 using Pkg
+Pkg.activate(".")
+Pkg.update()
 using PrecompileTools, Preferences
 set_preferences!(PrecompileTools, "precompile_workloads" => false; force=true)
-Pkg.update()
 Pkg.test()
 
