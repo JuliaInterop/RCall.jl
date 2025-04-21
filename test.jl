4139a1da9a60fb06d3f86d150b43cb16e77b1ef9
Phillip Alday <me@phillipalday.com>
2025-04-21 17:56:25 -0500

    test updates

--- test.jl
+++ test.jl
@@ -3,5 +3,6 @@ Pkg.activate(".")
 Pkg.update()
 using PrecompileTools, Preferences
 set_preferences!(PrecompileTools, "precompile_workloads" => false; force=true)
+Pkg.build()
 Pkg.test()
 
---
+++ test.sh
@@ -0,0 +1,1 @@
+R_HOME='*' JULIA_LOAD_PATH="@:@stdlib" julia +dev --startup-file=no --project test.jl
