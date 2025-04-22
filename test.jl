c7a952e6305547230537e5b4f312342b5da809f1
Phillip Alday <me@phillipalday.com>
2025-04-22 00:49:19 -0500

    more bisection tricks

--- test.jl
+++ test.jl
@@ -1,8 +1,10 @@
 using Pkg
+Pkg.UPDATED_REGISTRY_THIS_SESSION[] = true
 Pkg.activate(".")
 Pkg.update()
 using PrecompileTools, Preferences
 set_preferences!(PrecompileTools, "precompile_workloads" => false; force=true)
+Pkg.update()
 Pkg.build()
 Pkg.test()
 
