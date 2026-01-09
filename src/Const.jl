0b61e78d2f945ec7ce8250100bbdb8799a6e2771
Phillip Alday <me@phillipalday.com>
2026-01-09 13:02:33 -0600

    formatting improvements
    Co-authored-by: Alex Arslan <ararslan@comcast.net>

--- src/Const.jl
+++ src/Const.jl
@@ -1,13 +1,6 @@
 module Const
 
-using ..RCall: libR
-using ..RCall: RObject
-using ..RCall: NilSxp
-using ..RCall: SymSxp
-using ..RCall: EnvSxp
-using ..RCall: SxpHead
-using ..RCall: StrSxp
-using ..RCall: CharSxp
+using ..RCall: libR, RObject, NilSxp, SymSxp, EnvSxp, SxpHead, StrSxp, CharSxp
 
 const NaInt = typemin(Cint)
 const NaReal = reinterpret(Float64, 0x7ff00000000007a2)
@@ -17,39 +10,39 @@ const BlankString = RObject{CharSxp}()
 
 const BlankScalarString = RObject{StrSxp}()
 
-const BaseSymbol = RObject{SymSxp}() # base
-const BraceSymbol = RObject{SymSxp}() # {
-const Bracket2Symbol = RObject{SymSxp}() # [[
-const BracketSymbol = RObject{SymSxp}() # [
-const ClassSymbol = RObject{SymSxp}() # class
-const DeviceSymbol = RObject{SymSxp}() # .Device
-const DimNamesSymbol = RObject{SymSxp}() # dimnames
-const DimSymbol = RObject{SymSxp}() # dim
-const DollarSymbol = RObject{SymSxp}() # $
-const DotsSymbol = RObject{SymSxp}() # ...
+const BaseSymbol = RObject{SymSxp}()        # base
+const BraceSymbol = RObject{SymSxp}()       # {
+const Bracket2Symbol = RObject{SymSxp}()    # [[
+const BracketSymbol = RObject{SymSxp}()     # [
+const ClassSymbol = RObject{SymSxp}()       # class
+const DeviceSymbol = RObject{SymSxp}()      # .Device
+const DimNamesSymbol = RObject{SymSxp}()    # dimnames
+const DimSymbol = RObject{SymSxp}()         # dim
+const DollarSymbol = RObject{SymSxp}()      # $
+const DotsSymbol = RObject{SymSxp}()        # ...
 const DoubleColonSymbol = RObject{SymSxp}() # ::
-const DropSymbol = RObject{SymSxp}() # drop
-const LastvalueSymbol = RObject{SymSxp}() # .Last.value
-const LevelsSymbol = RObject{SymSxp}() # levels
+const DropSymbol = RObject{SymSxp}()        # drop
+const LastvalueSymbol = RObject{SymSxp}()   # .Last.value
+const LevelsSymbol = RObject{SymSxp}()      # levels
 const MissingArg = RObject{SymSxp}()
-const ModeSymbol = RObject{SymSxp}() # mode
-const NaRmSymbol = RObject{SymSxp}() # na.rm
-const NameSymbol = RObject{SymSxp}() # name
-const NamesSymbol = RObject{SymSxp}() # names
+const ModeSymbol = RObject{SymSxp}()        # mode
+const NaRmSymbol = RObject{SymSxp}()        # na.rm
+const NameSymbol = RObject{SymSxp}()        # name
+const NamesSymbol = RObject{SymSxp}()       # names
 const NamespaceEnvSymbol = RObject{SymSxp}() # .__NAMESPACE__.
-const PackageSymbol = RObject{SymSxp}() # package
-const PreviousSymbol = RObject{SymSxp}() # previous
-const QuoteSymbol = RObject{SymSxp}() # quote
-const RowNamesSymbol = RObject{SymSxp}() # row.names
-const SeedsSymbol = RObject{SymSxp}() # .Random.seed
-const SortListSymbol = RObject{SymSxp}() # sort.lis
-const SourceSymbol = RObject{SymSxp}() # source
-const SpecSymbol = RObject{SymSxp}() # spec
+const PackageSymbol = RObject{SymSxp}()     # package
+const PreviousSymbol = RObject{SymSxp}()    # previous
+const QuoteSymbol = RObject{SymSxp}()       # quote
+const RowNamesSymbol = RObject{SymSxp}()    # row.names
+const SeedsSymbol = RObject{SymSxp}()       # .Random.seed
+const SortListSymbol = RObject{SymSxp}()    # sort.lis
+const SourceSymbol = RObject{SymSxp}()      # source
+const SpecSymbol = RObject{SymSxp}()        # spec
 const TripleColonSymbol = RObject{SymSxp}() # :::
-const dot_defined = RObject{SymSxp}() # .defined
-const dot_Method = RObject{SymSxp}() # .Method
-const dot_packageName = RObject{SymSxp}() # .packageName
-const dot_target = RObject{SymSxp}() # .target
+const dot_defined = RObject{SymSxp}()       # .defined
+const dot_Method = RObject{SymSxp}()        # .Method
+const dot_packageName = RObject{SymSxp}()   # .packageName
+const dot_target = RObject{SymSxp}()        # .target
 
 const EmptyEnv = RObject{EnvSxp}()
 const GlobalEnv = RObject{EnvSxp}()
